6c7c58726e3b71e4ef7639f709854e77d02b025f
vijayalakshmi kaja <61687772+kvijaya-datascientist@users.noreply.github.com>
2021-07-19 17:50:42 +0530

    adding example

--- scripts/Data Structures/Arrays/ReverseStringTest4.py
+++ scripts/Data Structures/Arrays/ReverseStringTest4.py
@@ -1,19 +1,28 @@
-"""
+
+
 class ReverseString:
 
     def reverse(self,str):
+        if(len(str) < 2 ):
+         return "Hmmm..., This is not good"
         temp_str = []
         for i in range(len(str)-1 , -1 , -1):
-            temp_str.append(str[i])
+         temp_str.append(str[i])
         return ''.join(temp_str)
 
 if __name__ == "__main__":
     ins1 = ReverseString()
-    input = input("Enter the statement -->  ")
-    reverse_str = ins1.reverse(input)
-    print(reverse_str)  """
+    input_str = input("Enter the statment/wor for reversing -->  ")
+    rev_str  = ins1.reverse(input_str)
+    print(rev_str)
 
-"""from py.builtin import reversed
-name = "hello"
-a = reversed(name)
-print(''.join(a))  """
+# using built-in methods
+#input_str = 'He is good boy'
+print(input_str[-1::-1])
+print(''.join(reversed(input_str)))
+
+list1 = list(input_str)
+list1.reverse()
+print(''.join(list1))
+
+
